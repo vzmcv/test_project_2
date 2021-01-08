3443bff9e3b1c2769bfcc392733d385313fd4fac
vzmcv <Ruzic_678>
2021-01-08 17:45:10 +0300

    UPDATE TEST

--- src/test/java/com/example/Test_Project_2/MainPageTest.java
+++ src/test/java/com/example/Test_Project_2/MainPageTest.java
@@ -1,21 +1,16 @@
 package com.example.Test_Project_2;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class MainPageTest {
-    private final MainPage mainPage = new MainPage();
 
     @BeforeAll
     public static void setUpAllure() {
@@ -31,7 +26,13 @@ public class MainPageTest {
     @Test
     public void search() {
 
-        $(byTitle("Search")).sendKeys("Selenium practice");
+        $(byTitle("Search")).sendKeys(("Selenium practice"));
+
+        $(byXpath("//*[@type='submit']")).shouldHave(attribute("value", "Google Search")).click();
+
+        $(byId("center_col")).findElement(byXpath("//*[@href='https://www.seleniumeasy.com/test/']")).click();
+
 
     }
+
 }
